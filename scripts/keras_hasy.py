6bb1f526862ce75d64c9d7bb32ab806cbcb7db21
Zaid Alyafeai <alyafey22@gmail.com>
2018-06-07 17:44:19 +0300

    Update keras_hasy.py

--- scripts/keras_hasy.py
+++ scripts/keras_hasy.py
@@ -50,8 +50,10 @@ x_test = ht.preprocess(x_test)
 model = Sequential()
 model.add(Convolution2D(32, (3, 3),
                         padding='same',
-                        input_shape=x_train.shape[1:], activation='relu'))
-model.add(Convolution2D(64, (3, 3), padding='same', activation='relu'))
+                        input_shape=x_train.shape[1:]))
+model.add(PReLU())
+model.add(Convolution2D(64, (3, 3), padding='same'))
+model.add(PReLU())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
